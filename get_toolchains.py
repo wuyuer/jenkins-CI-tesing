f608fc1b91570754f8bd19d23e8c6fbb0e9819b7
Kevin Hilman <khilman@linaro.org>
2013-06-18 14:43:42 -0700

    get_toolchain: initial checkin

---
+++ get_toolchains.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import ftplib
+
+gcc_ver='4.6.3'
+ftp = ftplib.FTP('www.kernel.org')
+ftp.login()
+ftp.cwd('pub/tools/crosstool/files/bin/x86_64/%s' %gcc_ver)
+files = ftp.nlst()
+for f in files: 
+    if f[-3:] == '.xz':
+        print "Retrieving", f
+        ftp.retrbinary('RETR %s' %f, open(f, 'wb').write)
+        break
+
+ftp.quit()
