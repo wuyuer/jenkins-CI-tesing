37fb8d82d37baf18502146708080c0b2e558de25
Kevin Hilman <khilman@linaro.org>
2013-09-12 10:58:51 -0700

    util: tree_branch name: use GIT_BRANCH if it exists

--- util.py
+++ util.py
@@ -5,14 +5,18 @@ def get_header_info(base=''):
     describe = None
     commit = None
 
+    if os.environ.has_key('GIT_BRANCH'):
+        tree_branch = os.environ['GIT_BRANCH']
+
     if os.path.exists('.git'):
         describe = subprocess.check_output('git describe', shell=True).rstrip()
         commit = subprocess.check_output('git log -n1 --oneline --abbrev=10',
                                          shell=True)
-        tree_branch = subprocess.check_output('git describe --all',
-                                              shell=True).rstrip()
-        i = tree_branch.find('/')
-        if i > 0:
-            tree_branch = tree_branch[i+1:]
+        if not tree_branch:
+            tree_branch = subprocess.check_output('git describe --all',
+                                                  shell=True).rstrip()
+            i = tree_branch.find('/')
+            if i > 0:
+                tree_branch = tree_branch[i+1:]
 
     return (tree_branch, describe, commit)
