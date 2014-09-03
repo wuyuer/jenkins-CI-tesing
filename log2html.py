04ebd28a39ebbbbb5c13a379945d0327bf359513
Kevin Hilman <khilman@linaro.org>
2014-09-03 08:00:59 -0700

    log2html: add boot result, highlight pyboot messages in yellow

--- log2html.py
+++ log2html.py
@@ -6,6 +6,7 @@ import os
 import sys
 import subprocess
 import json
+import re
 
 headers = """
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
@@ -17,6 +18,7 @@ headers = """
   pre warn { color: #F88017; }
   pre err { color: red; }
   pre pass { color: green; }
+  pre pyboot { color: yellow; }
   pre offline { color: darkgray; }
   A:link {text-decoration: none }
   A:visited {text-decoration: none }
@@ -50,8 +52,12 @@ boot_json["boot_log_html"] = os.path.basename(html)
 json.dump(boot_json, json_f, indent=4, sort_keys=True)
 json_f.close()
 
+boot_result = None
+if boot_json.has_key("boot_result"):
+    boot_result = boot_json["boot_result"]
+
 html_f.write(headers %board)
-html_f.write("<h1>Boot log: %s</h1>\n" %board)
+html_f.write("<h1>Boot log: %s</h1>\n" %(board))
 
 errors = subprocess.check_output('grep "^\[ERR\]" %s | cat' %log, shell=True).splitlines()
 num_errors = len(errors)
@@ -60,13 +66,21 @@ num_warnings = len(warnings)
 
 html_f.write("<font size=-2><pre>\n")
 
-html_f.write("<h2>Errors: %d</h2>\n" %num_errors)
+html_f.write("<h2>Boot result: ")
+if boot_result == "PASS":
+    html_f.write("<pass>PASS</pass></h2>")
+elif boot_result == "FAIL":
+    html_f.write("<err>FAIL</err></h2>")
+else:
+    html_f.write("<warn>%s</warn></h2>" %boot_result)
+
+html_f.write("<h2>Errors: %d</h2>" %num_errors)
 if num_errors:
     for e in errors:
-        html_f.write("<err>%s</err>\n" %e.rstrip())
+        html_f.write("<err>%s</err>" %e.rstrip())
     html_f.write("\n")
 
-html_f.write("<h2>Warnings: %d</h2>\n" %num_warnings)
+html_f.write("<h2>Warnings: %d</h2>" %num_warnings)
 if num_warnings:
     for e in warnings:
         html_f.write("<warn>%s</warn>\n" %e.rstrip())
@@ -74,13 +88,16 @@ if num_warnings:
 
 html_f.write("<h2>Full boot log:</h2>\n")
 for line in log_f:
-    warn = err = False
+    warn = err = pyboot = False
     if line.startswith("[WARN]"):
         warn = True
         html_f.write("<warn>")
     elif line.startswith("[ERR]"):
         err = True
         html_f.write("<err>")
+    m = re.search("^# PYBOOT:", line)
+    if m:
+        line = "<pyboot>%s</pyboot>" %line
     html_f.write(line)
     if warn:
         html_f.write("</warn>")
